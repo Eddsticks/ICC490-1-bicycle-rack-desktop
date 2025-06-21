8ef1c31c8694f4be04bfa247e0b8eaa4932dea05
Eduardo <ewaldo.jefe@gmail.com>
2025-06-21 01:11:20 -0400

    necesario

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module com.icc490 {
     requires javafx.graphics;-bici.desktop {
     requires javafx.controls;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.databind;
+    requires java.net.http;
     exports com.icc490-bici.desktop;
 }
